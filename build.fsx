42d0457c19af42d47ca6cd3df8afb72d5ef47d1d
Sándor Rakonczai <lunnia@inf.elte.hu>
2015-08-06 14:51:06 +0200

    Add build scripts

---
+++ build.fsx
@@ -0,0 +1,42 @@
+#load "tools/includes.fsx"
+
+open IntelliFactory.Build
+
+let bt =
+    BuildTool().PackageId("WebSharper.MaterialUI")
+        .VersionFrom("WebSharper")
+        .WithFramework(fun x -> x.Net40)
+
+let library =
+    bt.WebSharper.Library("WebSharper.MaterialUI")
+        .SourcesFromProject()
+        .References(fun ref ->
+            [
+                ref.NuGet("WebSharper.React").Reference()
+            ])
+
+let tests =
+    bt.WebSharper.BundleWebsite("WebSharper.MaterialUI.Tests")
+        .SourcesFromProject()
+        .References(fun ref -> 
+            [
+                ref.NuGet("WebSharper.React").Reference()
+                ref.Project library
+            ])
+
+bt.Solution [
+    library
+    tests
+
+    bt.NuGet.CreatePackage()
+        .Configure(fun configuration ->
+            { configuration with
+                Title = Some "WebSharper.MaterialUI"
+                LicenseUrl = Some "http://websharper.com/licensing"
+                ProjectUrl = Some "https://bitbucket.com/intellifactory/websharper.materialui"
+                Description = "WebSharper bindings for Material UI"
+                Authors = [ "IntelliFactory" ]
+                RequiresLicenseAcceptance = true })
+        .Add(library)
+]
+|> bt.Dispatch
