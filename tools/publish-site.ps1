b481f4b5f0545fe88108ce26b096e7eb8c9e8bbb
Loic Denuziere <loic@denuziere.net>
2018-08-15 18:34:45 +0200

    Add AppVeyor configuration

---
+++ tools/publish-site.ps1
@@ -0,0 +1,9 @@
+# creates build/html
+rm -r build -errorAction ignore
+$d = mkdir build
+$d = mkdir build/html
+cp -r WebSharper.MaterialUI.Tests/Content build/html/
+cp -r WebSharper.MaterialUI.Tests/*.jpg build/html/
+cp -r WebSharper.MaterialUI.Tests/*.css build/html/
+cp -r WebSharper.MaterialUI.Tests/*.html build/html/
+cp -r WebSharper.MaterialUI.Tests/Resources build/html/
