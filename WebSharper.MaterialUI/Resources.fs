876100e392abc587d56611108fbc6ffb3cb70b90
AppVeyor <websharper-support@intellifactory.com>
2018-08-15 11:05:32 -0700

    gh-pages.ps1: build/html -> gh-pages

--- WebSharper.MaterialUI/Resources.fs
+++
@@ -1,9 +0,0 @@
-﻿namespace WebSharper.MaterialUI
-
-open WebSharper
-
-[<AutoOpen>]
-module Resources =
-    
-    type MaterialUI() =
-        inherit Resources.BaseResource("WebSharper.MaterialUI.material-ui.min.js")
--- tools/publish-site.ps1
+++
@@ -1,9 +0,0 @@
-# creates build/html
-rm -r build -errorAction ignore
-$d = mkdir build
-$d = mkdir build/html
-cp -r WebSharper.MaterialUI.Tests/Content build/html/
-cp -r WebSharper.MaterialUI.Tests/*.jpg build/html/
-cp -r WebSharper.MaterialUI.Tests/*.css build/html/
-cp -r WebSharper.MaterialUI.Tests/*.html build/html/
-cp -r WebSharper.MaterialUI.Tests/Resources build/html/
